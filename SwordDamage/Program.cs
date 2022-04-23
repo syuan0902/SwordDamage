b88a4c1d51f783a20016a5f420875e0304ec72e0
syuan7492 <kurosaki7492@gmail.com>
2022-04-23 23:17:55 +0800

    Add project files.

---
+++ SwordDamage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SwordDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamageCount m_swordDamageCount = new SwordDamageCount();
+            Random m_random = new Random();
+
+            while (true)
+            {
+                Console.Write("普通攻擊請輸入0, 魔法攻擊請按1, 火焰劍攻擊請按2, 火焰劍附加魔法攻擊請按3, 離開測試請輸入其他按鍵: ");
+
+                char m_Input = Console.ReadKey().KeyChar;
+
+                if (m_Input != '0' && m_Input != '1' && m_Input != '2' && m_Input != '3') return;
+
+                int m_3Roll = m_random.Next(1, 7) + m_random.Next(1, 7) + m_random.Next(1, 7);
+
+                m_swordDamageCount.m_roll = m_3Roll;
+
+                m_swordDamageCount.SetMagic(m_Input == '1' || m_Input == '3');
+
+                m_swordDamageCount.SetFlameSword(m_Input == '2' || m_Input == '3');
+
+                Console.WriteLine("\n3顆骰子擲出了 " + m_3Roll + "總傷害為 " + m_swordDamageCount.m_totalDamage + "HP\n");
+            }
+        }
+    }
+}
---
+++ SwordDamage/SwordDamageCount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwordDamage
+{
+    class SwordDamageCount
+    {
+
+        public const int m_baseDamage = 3;
+        public const int m_flameSwordDamage = 2;
+
+        public int m_roll;
+        public decimal m_magicMultiplier = 1m;
+        public int m_flameDamage = 0;
+        public int m_totalDamage;
+
+        public void CalculateDamage()
+        {
+            m_totalDamage = (int)(m_roll * m_magicMultiplier) + m_baseDamage + m_flameDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                m_magicMultiplier = 1.75m;
+            }
+
+            else
+            {
+                m_magicMultiplier = 1m;
+            }
+
+            CalculateDamage();
+        }
+
+        public void SetFlameSword(bool isFlameSword)
+        {
+            CalculateDamage();
+
+            if (isFlameSword)
+            {
+                m_totalDamage += m_flameSwordDamage;
+            }
+        }
+    }
+}
